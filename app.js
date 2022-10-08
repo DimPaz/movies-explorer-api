23500e55ca6bd6d7394e22c645db9da86d8f5a34
Dmitry Pazuxin <dpazuxin@bk.ru>
2022-10-08 17:52:52 +0300

    movies-explorer-api: подключен helmet и настроен rate limiter

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet"); // шлем
+const limiter = require("./middlewares/riteLimiter");
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 const { dataRouter } = require("./routes/index");
-const { errors } = require('celebrate');
+const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/error");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 // console.log(require('crypto').randomBytes(32).toString('hex'));
 // app.use((req, res, next) => {
@@ -19,7 +21,19 @@ const errorHandler = require("./middlewares/error");
 //   next();
 // });
 
+app.use(requestLogger); // подключаем логгер запросов (самый первый обработчик должен быть)
+app.use(helmet()); // шлем
+app.use(limiter); // подключили ограничитель
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 app.use("/", dataRouter);
+
+app.use(errorLogger); // подключаем логгер ошибок
 
 // обработчики ошибок
 app.use(errors()); // обработчик ошибок celebrate
---
+++ middlewares/riteLimiter.js
@@ -0,0 +1,10 @@
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 900000, // 15 минут
+  max: 100, // Ограничьте каждый IP-адрес 100 запросами на "окно" за 15 минут
+  standardHeaders: true, // Возвращает информацию об ограничении скорости в заголовках `RateLimit-*`
+  legacyHeaders: false, // Отключить заголовки `X-RateLimit-*`
+});
+
+module.exports = limiter;
